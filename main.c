c6f2ea03c1e8f70adb2750457051a5873a63a5b5
kojurkin <kajukinia@stud.omsu.ru>
2022-03-25 20:05:52 +0600

    Initial commit

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+double random (int lower_range_limit, int upper_range_limit)
+{
+    double result;
+    result = rand() % (upper_range_limit - lower_range_limit) + lower_range_limit + (1. / rand());
+    return result;
+}
+
+int main()
+{
+    srand(time(0));
+    int count_num;
+    int lower_range_limit, upper_range_limit;
+    double randomres;
+    printf("Enter count of nums\n");
+    scanf("%d", &count_num);
+    printf("Enter Lower and upper range limits\n");
+    scanf("%d %d", &lower_range_limit, &upper_range_limit);
+    FILE *out_file = fopen("output.txt", "w");
+    for (int i = 0; i < count_num; i++)
+    {
+        int j = i + 1;
+        randomres = random(lower_range_limit, upper_range_limit);
+        printf("%dnum = %lf\n", j, randomres);
+        fprintf(out_file, "%lf\n ", randomres);
+    }
+    fclose(out_file);
+    return 0;
+}
